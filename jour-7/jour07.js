91ff41dcd851eaedcb0964c4da22604641a17792
MarvinAugust <marvin.auguste@hotmail.fr>
2022-02-03 14:14:57 +0100

    create commit

---
+++ jour-7/jour07.js
@@ -0,0 +1,39 @@
+// var fs = require("fs");
+// fs.readFile("jour07.txt", function (err, data) {
+//    if (err) {
+//       console.error(err);
+//       return;
+//    }
+
+//    console.log("Lecture en différé : " + data.toString());
+// });
+
+
+
+// let array = [1, 2, 3, 4, 5]
+
+// var doubles = array.map(function(number) {
+//     return number * 2;
+//   });
+  
+//   console.log(doubles);
+
+
+
+
+  let longNames = [
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+]
+
+var shortNames = array.map(function(number) {
+    return number * 2;
+  });
+  
+  console.log(shortNames);
