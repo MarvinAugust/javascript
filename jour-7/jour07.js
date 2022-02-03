a0d3ee52d10aea28c57100a9b70790bdb8531a24
MarvinAugust <marvin.auguste@hotmail.fr>
2022-02-03 16:06:51 +0100

    jour7 exercice

--- jour-7/jour07.js
+++ jour-7/jour07.js
@@ -15,25 +15,94 @@
 // var doubles = array.map(function(number) {
 //     return number * 2;
 //   });
-  
+
 //   console.log(doubles);
 
 
 
 
-  let longNames = [
-	{
-		firstName: "Jane",
-		lastName: "Doe"
-	},
-	{
-		firstName: "John",
-		lastName: "Smith"
-	}
-]
+//   let longNames = [
+// 	{
+// 		firstName: "Jane",
+// 		lastName: "Doe"
+// 	},
+// 	{
+// 		firstName: "John",
+// 		lastName: "Smith"
+// 	}
+// ]
 
-var shortNames = array.map(function(number) {
-    return number * 2;
-  });
-  
-  console.log(shortNames);
+// var shortNames = longNames.map(function(names) {
+//     // console.log(names.firstName + " " + names.lastName)
+//     let name = [{name: names.firstName + " " + names.lastName}];
+//     console.log(name);
+//     return names ;
+//   });
+
+//   console.log(name);
+
+
+
+// let array = [1, "Toto", 34, "javascript", 8];
+
+// var numbers = array.filter(function (tableau) {
+//     if (typeof tableau === "number") {
+//         return tableau;
+//     }
+// });
+
+// console.log(numbers);
+
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// let even = numbers.filter(function (num) {
+//     if (num % 2 === 0 ) {
+
+//     return num
+//     }
+// });
+
+// console.log(even);
+
+
+let cakes =
+    [
+        {
+            name: "cake",
+            flavor: "vanilla",
+            status: "available"
+        },
+        {
+            name: "brownie",
+            flavor: "chocolate",
+            status: "available"
+        },
+        {
+            name: "pie",
+            flavor: "strawberry",
+            status: "available"
+        },
+        {
+            name: "muffin",
+            flavor: "pistachio",
+            status: "available"
+        },
+        {
+            name: "donut",
+            flavor: "chocolate",
+            status: "available"
+        },
+    ]
+
+let sold = cakes.filter(function (cake) {
+    if (cake.flavor === "chocolate") {
+        cake.status = "Sold out"
+        // console.log(cake);
+        return true
+    } else { 
+        return false
+    }
+});
+
+console.log(sold);
